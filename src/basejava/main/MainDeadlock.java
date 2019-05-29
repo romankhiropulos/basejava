325b9a7bd9ee91d85b1b1fafcddf6765f10a1fc2
romankhiropulos <romankhiropulos@gmail.com>
2019-05-29 22:56:56 +0300

    I have done hw11 - deadlock and fixed anonymous classes in hw10

--- src/basejava/main/MainDeadlock.java
+++ src/basejava/main/MainDeadlock.java
@@ -1,51 +1,46 @@
 package basejava.main;
 
 
-public class MainDeadlock {
-    private static Thread THREAD_1;
-    private static Thread THREAD_2;
-    static class Printer{
+import java.util.Arrays;
 
-        public <T> void print(T[] items){
-            for(T item: items){
-                System.out.println(item);
-            }
-        }
-    }
+public class MainDeadlock {
+    private static Thread threadFirst;
+    private static Thread threadSecond;
 
     public static void main(String[] args) throws InterruptedException {
 
-        //Arrays.asList( "a", "b", "d" ).forEach(System.out::println);
+        String[] people = {"Tom", "Alice", "Sam"};
+        Integer[] numbers = {23, 4, 5, 2};
 
-        Printer printer = new Printer();
-        String[] people = {"Tom", "Alice", "Sam", "Kate", "Bob", "Helen"};
-        Integer[] numbers = {23, 4, 5, 2, 13, 456, 4};
-
-        THREAD_1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    THREAD_2.join();
-                    printer.<String>print(people);
-                    System.out.println("THREAD_1 finished");
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        THREAD_2 = new Thread(() -> {
+        threadFirst = new Thread(() -> {
             try {
-                THREAD_1.join();
-                printer.<Integer>print(numbers);
-                System.out.println("THREAD_2 finished");
+                joinThread(threadSecond, threadFirst, people);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        });
-        
-        THREAD_1.start();
-        //THREAD_1.join();  //deadlock
-        THREAD_2.start();
+        }, "Thread_First");
+
+        threadSecond = new Thread(() -> {
+            try {
+                joinThread(threadFirst, threadSecond, numbers);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "Thread_Second");
+
+        // DeadLock
+        threadFirst.start();
+
+        // Without deadlock
+        //threadFirst.join();
+
+        threadSecond.start();
+    }
+
+    private static <T> void joinThread(Thread threadJoin, Thread threadCurrent, T[] items) throws InterruptedException {
+        System.out.println(threadCurrent.getName() + " start");
+        threadJoin.join();
+        Arrays.asList(items).forEach(System.out::println);
+        System.out.println(threadCurrent.getName() + " finished");
     }
 }
