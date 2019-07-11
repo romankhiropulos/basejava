56391182521ca11b0efab23c874380e9830fa633
romankhiropulos <romankhiropulos@gmail.com>
2019-07-12 00:31:08 +0300

    I have add table section for HW15

--- config/init_db.sql
+++ config/init_db.sql
@@ -1,6 +1,6 @@
 CREATE TABLE resume (
-  uuid      CHAR(36) PRIMARY KEY NOT NULL,
-  full_name TEXT                 NOT NULL
+  uuid      CHAR(36) PRIMARY KEY,
+  full_name TEXT        NOT NULL
 );
 
 CREATE TABLE contact (
@@ -12,3 +12,14 @@ CREATE TABLE contact (
 
 CREATE UNIQUE INDEX contact_uuid_type_index
   ON contact (resume_uuid, type);
+
+CREATE TABLE section (
+  id          SERIAL,
+  resume_uuid CHAR(36) NOT NULL REFERENCES resume (uuid) ON DELETE CASCADE,
+  type        TEXT     NOT NULL,
+  value       TEXT     NOT NULL
+);
+
+CREATE UNIQUE INDEX section_uuid_type_index
+  ON section (resume_uuid, type);
+
